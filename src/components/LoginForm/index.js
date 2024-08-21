770ce47cf3adf25827c2954c0fdab48ea3025f4e
ashok <K.ashok1832@gmal.com>
2024-08-21 09:31:41 +0000

    Ashok

---
+++ src/components/LoginForm/index.js
@@ -0,0 +1,112 @@
+import {Component} from 'react'
+
+import './index.css'
+
+class LoginForm extends Component {
+  state = {
+    username: '',
+    password: '',
+  }
+  onLoginSuccess=()=> {
+    const {history}=this.props
+    console.log(history)
+    history.push("/")
+  }
+
+  onSubmitForm = async (event) => {
+    event.preventDefault()
+    const {username, password} = this.state
+    const loginDetails = {username, password}
+    const url = 'https://apis.ccbp.in/login'
+    const userDetails= {
+      method: "POST",
+      body: JSON.stringify(loginDetails)
+    }
+    
+    const response= await fetch(url, userDetails)
+    const data = response.json()
+    console.log(response)
+    
+    if (response.ok === true) {
+      this.onLoginSuccess()
+    }
+
+    
+
+  }
+
+  onChangeUsername = event => {
+    this.setState({username: event.target.value})
+  }
+
+  onChangePassword = event => {
+    this.setState({password: event.target.value})
+  }
+
+  renderPasswordField = () => {
+    const {password} = this.state
+    return (
+      <>
+        <label className="input-label" htmlFor="password">
+          PASSWORD
+        </label>
+        <input
+          type="password"
+          id="password"
+          className="password-input-filed"
+          value={password}
+          onChange={this.onChangePassword}
+        />
+      </>
+    )
+  }
+
+  renderUsernameField = () => {
+    const {username} = this.state
+    return (
+      <>
+        <label className="input-label" htmlFor="username">
+          USERNAME
+        </label>
+        <input
+          type="text"
+          id="username"
+          className="username-input-filed"
+          value={username}
+          onChange={this.onChangeUsername}
+        />
+      </>
+    )
+  }
+
+  render() {
+    return (
+      <div className="login-form-container">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-logo-img.png"
+          className="login-website-logo-mobile-image"
+          alt="website logo"
+        />
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-login-img.png"
+          className="login-image"
+          alt="website login"
+        />
+        <form className="form-container" onSubmit={this.onSubmitForm}>
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-logo-img.png"
+            className="login-website-logo-desktop-image"
+            alt="website logo"
+          />
+          <div className="input-container">{this.renderUsernameField()}</div>
+          <div className="input-container">{this.renderPasswordField()}</div>
+          <button type="submit" className="login-button">
+            Login
+          </button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default LoginForm
